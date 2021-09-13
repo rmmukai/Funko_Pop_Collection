9d5c88798d840ad8d64a34ef789c48d94ba94d12
Ronnie <rmmukai@gmail.com>
2021-09-13 14:39:47 -0700

    Third Commit: - Added CSS stylesheet "pop_entry.css" and tested to make sure it works.

--- Pop_App/forms.py
+++ Pop_App/forms.py
@@ -5,9 +5,13 @@ from .models import *
 class PopEntryForm(forms.ModelForm):
     class Meta:
         model = Pop_Entry
-        fields = [
+        fields = (
             'character',
             'series',
             'series_number',
             'special_edition'
-        ]
+        )
+
+        widgets = {
+            'character': forms.TextInput(attrs={'class': 'characters'}),
+        }
